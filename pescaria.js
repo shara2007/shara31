7e6e4e505d3fb24cabbf2bac9b22c29eab39c32a
shara2007 <126672559+shara2007@users.noreply.github.com>
2023-11-13 11:42:09 -0300

    Add files via upload

--- pescaria.js
+++ pescaria.js
@@ -8,13 +8,13 @@ frm.addeventlistener("submit", (e)=>{
 
     const pessoas = number(frm.inNumero.value)
     const peixe = number(frm.inPeixe.value)
-    ler resultado
+    let= resultado
     if(peixe > pessoas){
         resultado = (pessoas * 20) + ((peixe - pessoas)*12)
     } else{
         resultado = pessoas * 20
     }
-    resp.innertext= `N de pessoas:${pessoas}\n
+    resp.innerText= `N de pessoas:${pessoas}\n
                      N de peixes: ${peixe}\n
                      pagar:R$ ${resultado.tofixed(2)}`
 })
