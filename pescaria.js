f2a0c0be37ef5a60d5f1844f6a447d81d1af2a0a
shara2007 <126672559+shara2007@users.noreply.github.com>
2023-11-13 11:52:33 -0300

    13/11

--- pescaria.js
+++ pescaria.js
@@ -1,20 +1,21 @@
-const frm = document.queryselector("form")
-const resp = document.queryselector("h4")
+const frm = document.querySelector("form")
+const resp = document.querySelector("h4")
 
 //ouvinte
 
-frm.addeventlistener("submit", (e)=>{
-    e. preventdefault()
+frm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    
 
-    const pessoas = number(frm.inNumero.value)
-    const peixe = number(frm.inPeixe.value)
-    let= resultado
+    const pessoas = Number(frm.inNumero.value)
+    const peixe = Number(frm.inPeixe.value)
+    let resultado
     if(peixe > pessoas){
         resultado = (pessoas * 20) + ((peixe - pessoas)*12)
     } else{
         resultado = pessoas * 20
     }
-    resp.innerText= `N de pessoas:${pessoas}\n
-                     N de peixes: ${peixe}\n
-                     pagar:R$ ${resultado.tofixed(2)}`
+    resp.innerText= `N° de pessoas:${pessoas}\n
+                     N° de peixes: ${peixe}\n
+                     pagar: R$ ${resultado.toFixed(2)}`
 })
